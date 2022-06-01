27d849397d771d7b901287f5f02b3ff37b891131
atmason90 <atmason90@gmail.com>
2022-06-01 00:04:07 -0700

    edit home component

--- portfolio-react/src/components/Home.jsx
+++ portfolio-react/src/components/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
       
       {/* container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#ff66c4]'>Hi, my name is</p>
+        <p className='text-[#ff66c4] text-lg'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Andrew Mason</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a full stack developer</h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>with a background in Management Consulting. Looking to leverage my development skills to provide web applications that support business initiatives. Earned a certificate in web development from UC Berkeley.</p>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>with a background in Management Consulting. Looking to leverage my development skills to provide web applications that support business initiatives.</p>
       
       <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ff66c4] hover:border-[#ff66c4]'>View Projects <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span></button>
