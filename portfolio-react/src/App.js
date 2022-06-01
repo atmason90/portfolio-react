228bce4ab4ec98cd867846e8bd74ac8d252482d0
atmason90 <atmason90@gmail.com>
2022-06-01 01:45:56 -0700

    start projects component

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 
 
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Resume />
+      <Projects />
     </div>
   );
 }
