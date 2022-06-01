5a5c10c24bb8e31317cec8bae0a128909d417312
atmason90 <atmason90@gmail.com>
2022-06-01 16:57:33 -0700

    add links to projects component

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -28,10 +28,10 @@ const Projects = () => {
                         Bookend Application
                     </span>
                     <div className='pt-8 text-center'>
-                       <a href="/">
-                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Deployed</button>
+                       <a href="https://glacial-plateau-74113.herokuapp.com/" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a> 
-                        <a href="/">
+                        <a href="https://github.com/atmason90/bookend" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
                         </a> 
                     </div>
@@ -44,13 +44,13 @@ const Projects = () => {
                 {/* hover effects */}
                 <div className='opacity-0 group-hover:opacity-100'>
                     <span className='text-2xl justify-center font-bold text-white tracking-wider'>
-                        Tech Blog
+                        Tech Blog App
                     </span>
                     <div className='pt-8 text-center'>
-                       <a href="/">
-                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Deployed</button>
+                       <a href="https://the-tech-blog-am.herokuapp.com/" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a> 
-                        <a href="/">
+                        <a href="https://github.com/atmason90/tech-blog-app" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
                         </a> 
                     </div>
@@ -63,13 +63,13 @@ const Projects = () => {
                 {/* hover effects */}
                 <div className='opacity-0 group-hover:opacity-100'>
                     <span className='text-2xl justify-center font-bold text-white tracking-wider'>
-                        Text Editor
+                        Text Editor PWA
                     </span>
                     <div className='pt-8 text-center'>
-                       <a href="/">
-                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Deployed</button>
+                       <a href="https://editmytext.herokuapp.com/" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a> 
-                        <a href="/">
+                        <a href="https://github.com/atmason90/text-editor" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
                         </a> 
                     </div>
@@ -85,10 +85,10 @@ const Projects = () => {
                         Social Network API
                     </span>
                     <div className='pt-8 text-center'>
-                       <a href="/">
-                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Deployed</button>
+                       <a href="https://user-images.githubusercontent.com/99947655/169758282-4292a54e-8e30-4d30-ab04-9b888abc0745.mp4" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a> 
-                        <a href="/">
+                        <a href="https://github.com/atmason90/social-network-api" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
                         </a> 
                     </div>
@@ -104,10 +104,10 @@ const Projects = () => {
                         E-commerce Backend
                     </span>
                     <div className='pt-8 text-center'>
-                       <a href="/">
-                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Deployed</button>
+                       <a href="https://drive.google.com/file/d/1KBD6tlB82vbEWURTLGDzOHshITIDAft2/view?usp=sharing" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a> 
-                        <a href="/">
+                        <a href="https://github.com/atmason90/e-comm-back-end" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
                         </a> 
                     </div>
@@ -120,13 +120,13 @@ const Projects = () => {
                 {/* hover effects */}
                 <div className='opacity-0 group-hover:opacity-100'>
                     <span className='text-2xl justify-center font-bold text-white tracking-wider'>
-                        Employee Management System
+                        Employee Mgmt. System
                     </span>
                     <div className='pt-8 text-center'>
-                       <a href="/">
+                       <a href="https://user-images.githubusercontent.com/99947655/166392001-e153fc6e-ddc9-4334-8ab9-fb791541c65e.mp4" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Deployed</button>
                         </a> 
-                        <a href="/">
+                        <a href="https://github.com/atmason90/employee-management-system" target='#'>
                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
                         </a> 
                     </div>
