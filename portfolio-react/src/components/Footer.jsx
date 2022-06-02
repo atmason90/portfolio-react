ea89f79fe9be217354e98067653df36323f5a601
atmason90 <atmason90@gmail.com>
2022-06-01 19:32:07 -0700

    build footer component

---
+++ portfolio-react/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+      <div>
+        <footer className='p-4 bg-[#0a192f] sm:p-6'>
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/atmason90" class="hover:underline">Andrew Mason</a>. All Rights Reserved.
+                </span>
+            </div>
+        </footer>
+      </div>
+  )
+}
+
+export default Footer
