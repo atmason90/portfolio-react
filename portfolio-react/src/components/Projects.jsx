ce133c55b919fb22371c638c4ffaec80ad10d6ad
atmason90 <atmason90@gmail.com>
2022-06-19 13:40:33 -0600

    add hungry sharks project

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -5,6 +5,7 @@ import editor from '../assets/texteditor.jpeg';
 import social from '../assets/socialnetwork.jpeg';
 import ecomm from '../assets/ecomm.jpeg';
 import ems from '../assets/ems.jpeg';
+import hungrysharks from '../assets/hungrysharks.gif';
 
 
 const Projects = () => {
@@ -18,6 +19,25 @@ const Projects = () => {
 
         {/* container */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+             {/* grid item */}
+             <div style={{ backgroundImage: `url(${hungrysharks})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                
+                {/* hover effects */}
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl justify-center font-bold text-white tracking-wider'>
+                        Hungry Sharks
+                    </span>
+                    <div className='pt-8 text-center'>
+                       <a href="https://hungryshark.herokuapp.com/" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        </a> 
+                        <a href="https://github.com/atmason90/hungry-sharks" target='#'>
+                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repo</button>
+                        </a> 
+                    </div>
+                </div>
+            </div>
             
             {/* grid item */}
             <div style={{ backgroundImage: `url(${bookend})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
