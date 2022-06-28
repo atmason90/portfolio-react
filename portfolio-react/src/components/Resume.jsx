4ab0ab400e6d088e246ce5f09f15795d7dee027f
atmason90 <atmason90@gmail.com>
2022-06-27 22:41:21 -0700

    update resume component with additional skills

--- portfolio-react/src/components/Resume.jsx
+++ portfolio-react/src/components/Resume.jsx
@@ -12,28 +12,68 @@ const Resume = () => {
             </div>
             <div></div>
             </div>
-            <div className='text-xl mb-4'>
-                <p>Here are some of the technologies I am most familiar with</p>
+            <div className='text-3xl mb-4'>
+                <p>Technologies and Skills</p>
             </div>
-        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-xl'>
-                <p className='font-bold'>Frontend:</p>
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-4 gap-8 px-4'>
+        <div className='text-xl'>
+                <p className='font-bold text-3xl'>Languages:</p>
                 <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>JQuery</li>
-                    <li>React</li>
+                    <li>JavaScript (ES6)</li>
+                    <li>HTML5</li>
+                    <li>Cascading Style Sheets (CSS)</li>
+                    <li>JSX</li>
                 </ul>
             </div>
             <div className='text-xl'>
-                <p className='font-bold'>Backend:</p>
+                <p className='font-bold text-3xl'>Frontend:</p>
                 <ul>
-                    <li>Express.js</li>
-                    <li>MySql</li>
-                    <li>MongoDB</li>
-                    <li>Rest APIs</li>
+                    <li>Bootstrap</li>
+                    <li>JQuery</li>
+                    <li>Moment.js</li>
+                    <li>Handlebars.js</li>
+                    <li>JWT JavaScript Web Token</li>
+                    <li>React</li>
+                    <li>React Hooks</li>
+                    <li>Webpack</li>
+                    <li>Progressive Web Apps (PWA)</li>
+                    <li>Context API</li>
+                </ul>
+            </div>
+            <div className='text-xl'>
+                <p className='font-bold text-3xl'>Backend:</p>
+                <ul>
+                    <li>Web API</li>
+                    <li>Server-Side API</li>
+                    <li>Third-Party API</li>
+                    <li>IndexedDB</li>
+                    <li>AJAX</li>
+                    <li>Fetch</li>
                     <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>Rest API</li>
+                    <li>MySql</li>
+                    <li>Sequelize ORM</li>
+                    <li>MongoDB</li>
+                    <li>Mongoose ODM</li>
+                    <li>Apollo GraphQL</li>
+                </ul>
+            </div>
+            <div className='text-xl'>
+                <p className='font-bold text-3xl'>Other:</p>
+                <ul>
+                    <li>Problem Solving</li>
+                    <li>Pseudocode</li>
+                    <li>Chrome Dev Tools</li>
+                    <li>Insomnia/Postman</li>
+                    <li>JSON</li>
+                    <li>Git</li>
+                    <li>GitHub</li>
+                    <li>Test Driven Development (TDD)</li>
+                    <li>Jest</li>
+                    <li>Data Structures</li>
+                    <li>Algorithms</li>
+                    <li>MERN Stack</li>
                 </ul>
             </div>
             <div>
